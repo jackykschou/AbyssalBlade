9343f1104fb3e795b69f770c257ce41c3886e2de
Jacky Chou <jacky.kschou@gmail.com>
2014-09-09 02:50:14 -0500

    added open scene window

---
+++ UnityProject/Assets/Scripts/Utility/Editor/SceneMenuItem.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+
+public class SceneMenuItem : Editor
+{
+    private const string SceneFolderPath = "Assets/Scenes/";
+    private const string SceneFileExtension = ".unity";
+
+    private const string TestSceneFolder = "Tests/";
+
+    [MenuItem("Open Scene/Testing/GameLogicComponentEventTest")]
+	public static void OpenGameLogicComponentEventTest()
+	{
+	    OpenScene(TestSceneFolder + "GameLogicComponentEventTest");
+	}
+
+    private static void OpenScene(string scenePath)
+    {
+        if (EditorApplication.SaveCurrentSceneIfUserWantsTo())
+        {
+            EditorApplication.OpenScene(SceneFolderPath + scenePath + SceneFileExtension);
+        }
+    }
+}
