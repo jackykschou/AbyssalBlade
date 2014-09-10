e50d3d3d0ae868466f270d1c99f4297990044d1b
Bsalch <Brandon.Salch@utexas.edu>
2014-09-09 20:40:14 -0500

    Added Walking animations

--- UnityProject/Assets/Scripts/CameraMovement.cs
+++ UnityProject/Assets/Scripts/CameraMovement.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class CameraMovement : MonoBehaviour {
 
-    float speed = 10.0f;
+    public float speed = 10.0f;
 
     public Animator a;
 
@@ -20,9 +20,54 @@ public class CameraMovement : MonoBehaviour {
         vert *= Time.deltaTime;
         horz *= Time.deltaTime;
         transform.Translate(horz, vert, 0);
-        if (Mathf.Abs(vert) > 0.01 || Mathf.Abs(horz) > 0.01)
-            a.SetBool("Moving", true);
+
+        if (Mathf.Abs(horz) > Mathf.Abs(vert))
+        {
+            if (horz > 0.01)
+            {
+                a.SetBool("Right", true);
+                a.SetBool("Left", false);
+                a.SetBool("Up", false);
+                a.SetBool("Down", false);
+            }
+            else
+            {
+                a.SetBool("Left", true);
+                a.SetBool("Up", false);
+                a.SetBool("Down", false);
+                a.SetBool("Right", false);
+            }
+        }
         else
+        {
+            if (vert > 0.01)
+            {
+                a.SetBool("Right", false);
+                a.SetBool("Left", false);
+                a.SetBool("Up", true);
+                a.SetBool("Down", false);
+            }
+            else
+            {
+                a.SetBool("Left", false);
+                a.SetBool("Up", false);
+                a.SetBool("Down", true);
+                a.SetBool("Right", false);
+            }
+        }
+
+
+        if (Mathf.Abs(vert) > 0.01 || Mathf.Abs(horz) > 0.01)
+        {
+            a.SetBool("Moving", true);
+        }
+        else
+        {
             a.SetBool("Moving", false);
+            a.SetBool("Right", false);
+            a.SetBool("Left", false);
+            a.SetBool("Up", false);
+            a.SetBool("Down", false);
+        }
 	}
 }
