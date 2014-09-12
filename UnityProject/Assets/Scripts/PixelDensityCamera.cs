1ae8835f79d2b660bf559dc9e9da5c6b3f44df16
Bsalch <Brandon.Salch@utexas.edu>
2014-09-12 00:53:27 -0500

    Added Pixel Density Camera and Width Scripts

---
+++ UnityProject/Assets/Scripts/PixelDensityCamera.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class PixelDensityCamera : MonoBehaviour {
+
+    public float pixelsToUnits = 10.0f;
+
+	void Update () {
+        camera.orthographicSize = Screen.height / pixelsToUnits / 2;
+	}
+}
