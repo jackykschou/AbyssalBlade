b4c4e6371941aeefccbca6f033553601036cd3d7
Bsalch <Brandon.Salch@utexas.edu>
2014-09-25 01:47:43 -0500

    Added Healthbar Prefab that mostly works and HealthBarController gamelogic. Character swings sped up

---
+++ UnityProject/Assets/Scripts/GameScripts/GameLogic/GUI/HealthBarController.cs
@@ -0,0 +1,61 @@
+﻿using Assets.Scripts.Attributes;
+using UnityEngine;
+using GameEvent = Assets.Scripts.Constants.GameEvent;
+using GameEventAttribute = Assets.Scripts.Attributes.GameEvent;
+using UnityEngine.UI;
+using Assets.Scripts.GameScripts.Components.GameValue;
+
+
+namespace Assets.Scripts.GameScripts.GameLogic
+{
+    public class HealthBarController : GameLogic
+    {
+        public Slider healthBarSlider;
+        public Text healthText;
+        GameValue hitPoints;
+
+        private float _startingHealth;
+        private float _damageTaken;
+        private float _damageHealed;
+
+        protected override void Initialize()
+        {
+ 	        base.Initialize();
+            hitPoints = this.gameObject.GetComponent<Destroyable.Destroyable>().HitPoint;
+            if(hitPoints == null)
+            {
+                Debug.Log("Could not find hit points");
+                return;
+            }
+            healthText.text = "100 %";
+            healthBarSlider.value = 100;
+            _startingHealth = (int)hitPoints.InitialValue;
+            _damageTaken = 0.0f;
+            _damageHealed = 0.0f;
+        }
+
+        [GameScriptEvent(Constants.GameScriptEvent.OnObjectTakeDamage)]
+        public void TakeDamage(float damage)
+        {
+            if(healthBarSlider == null)
+            {
+                Debug.Log("HealthBar Controller: Slider is NULL.");
+                return;
+            }
+            if (healthText == null)
+            {
+                Debug.Log("HealthBar Controller: Text is NULL.");
+                return;
+            }
+            if (healthBarSlider.value == 0)
+                return;
+            _damageTaken += (int)damage;
+            healthBarSlider.value = ((_startingHealth - _damageTaken) / _startingHealth) * 100;
+            healthText.text = ((int)healthBarSlider.value).ToString() + " %";
+        }
+
+        protected override void Deinitialize()
+        {
+        }
+    }
+}
