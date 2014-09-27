501a09e109b3202790af01cf4bef1afe8dc8ece1
Bsalch <Brandon.Salch@utexas.edu>
2014-09-27 15:17:26 -0500

    Refactored Hud Prefab, added extra cooldown bars that are hidden. Modified Skill cooldown script to have all skills

--- UnityProject/Assets/Scripts/GameScripts/GameLogic/GUI/SkillCoolDownSlider.cs
+++ UnityProject/Assets/Scripts/GameScripts/GameLogic/GUI/SkillCoolDownSlider.cs
@@ -9,7 +9,7 @@ namespace Assets.Scripts.GameScripts.GameLogic.GUI
     [RequireComponent(typeof(Slider))]
     public class SkillCoolDownSlider : GameLogic
     {
-        [Range(0, 3)] 
+        [Range(0, 4)] // 0 is movement skill
         public int SkillId;
         public Slider CooldownBar;
 
