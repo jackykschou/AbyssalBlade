54d344647bef1aaca44aadc1196a7b9ac705cf3b
Bsalch <Brandon.Salch@utexas.edu>
2014-09-29 12:25:23 -0500

    Starting Menu

---
+++ UnityProject/Assets/MouseFollow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseFollow : MonoBehaviour {
+    public float distance;
+    Light light;
+	// Use this for initialization
+	void Start () {
+        light = this.gameObject.GetComponent<Light>();
+        distance = -3.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 worldPoint = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,Input.mousePosition.y,distance));
+        light.transform.position = new Vector3(-worldPoint.x * Camera.main.aspect, -worldPoint.y * Camera.main.aspect, distance);
+        Debug.Log("X: " + worldPoint.x + "Y: " + worldPoint.y);
+	}
+}
