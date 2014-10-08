b2f9058833913b9bf84c695f751e7b47884032ac
Bsalch <Brandon.Salch@utexas.edu>
2014-10-08 16:23:52 -0500

    Updated Icons for correct abilities, added icon and highlight for dash

--- UnityProject/Assets/Scripts/GameScripts/GameLogic/ObjectMotor/TextMotor.cs
+++ UnityProject/Assets/Scripts/GameScripts/GameLogic/ObjectMotor/TextMotor.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 namespace Assets.Scripts.GameScripts.GameLogic.ObjectMotor
 {
     [AddComponentMenu("2DObjectMotor/TextMotor")]
+    [RequireComponent(typeof(Rigidbody2D))]
     public class TextMotor : ObjectMotor2D
     {
         public EaseType EaseType;
@@ -12,6 +13,9 @@ namespace Assets.Scripts.GameScripts.GameLogic.ObjectMotor
         public void Shoot(Vector3 dir, float speed, float distance)
         {
             MoveAlongWithStyle(EaseType, dir, speed, distance);
+            rigidbody2D.gravityScale = .1f;
+            rigidbody2D.isKinematic = false;
+            rigidbody2D.fixedAngle = true;
         }
     }
 }
