2f584aac9eb79c552cfc3f737d93d159f3b35691
Jacky Chou <jacky.kschou@gmail.com>
2014-10-22 14:57:09 -0500

    updated prefabs, updated player animator

--- UnityProject/Assets/Scripts/GameScripts/GameLogic/PhysicsBody/LeaveLevelAreaPhysicsBody.cs
+++ UnityProject/Assets/Scripts/GameScripts/GameLogic/PhysicsBody/LeaveLevelAreaPhysicsBody.cs
@@ -1,7 +1,9 @@
 ﻿using Assets.Scripts.Constants;
+using UnityEngine;
 
 namespace Assets.Scripts.GameScripts.GameLogic.PhysicsBody
 {
+    [AddComponentMenu("PhysicsBody/Projectile/LeaveLevelAreaPhysicsBody")]
     public class LeaveLevelAreaPhysicsBody : PhysicsBody2D
     {
         protected override void Initialize()
