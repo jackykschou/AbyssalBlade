a5f5d1a08d7911bd81e83a041f7d73fac7c7a7f9
Jacky Chou <jacky.kschou@gmail.com>
2014-10-22 14:24:00 -0500

    added different type trigger and collision finders

---
+++ UnityProject/Assets/Scripts/GameScripts/GameLogic/TargetFinders/OnCollisionExitFinder.cs
@@ -0,0 +1,27 @@
+﻿using Assets.Scripts.Attributes;
+using Assets.Scripts.GameScripts.GameLogic.PhysicsBody;
+using UnityEngine;
+
+namespace Assets.Scripts.GameScripts.GameLogic.TargetFinders
+{
+    [AddComponentMenu("TargetFinder/OnCollisionExitFinder")]
+    [RequireComponent(typeof(PhysicsBody2D))]
+    public class OnCollisionExitFinder : TargetFinder
+    {
+        protected override void Deinitialize()
+        {
+        }
+
+        protected override void FindTargets()
+        {
+        }
+
+        [GameScriptEvent(Constants.GameScriptEvent.OnPhysicsBodyOnCollisionExit2D)]
+        public void OnPhysicsBodyOnCollisionExit2D(Collision2D coll)
+        {
+            ClearTargets();
+            AddTarget(coll.gameObject);
+            ApplyEffects();
+        }
+    }
+}
