db639ce1cdf0d12a51565c979d71cda870260a29
Jacky Chou <jacky.kschou@gmail.com>
2014-10-28 12:59:12 -0500

    added ActivateByChance SkillEffectActivateCondition

--- UnityProject/Assets/Scripts/GameScripts/GameLogic/Skills/SkillEffects/ShakeCamera.cs
+++ UnityProject/Assets/Scripts/GameScripts/GameLogic/Skills/SkillEffects/ShakeCamera.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 
 namespace Assets.Scripts.GameScripts.GameLogic.Skills.SkillEffects
 {
+    [AddComponentMenu("Skill/SkillEffect/ShakeCamera")]
     public class ShakeCamera : SkillEffect
     {
-        [Range(0f, 1f)] public float ShakeIntensity;
+        [Range(0f, 1f)] 
+        public float ShakeIntensity;
 
         public override void Activate()
         {
