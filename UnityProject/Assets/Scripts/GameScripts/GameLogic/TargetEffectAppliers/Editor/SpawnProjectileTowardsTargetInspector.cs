22f4c30a40c0b70cf320b8905635486707656727
Jacky Chou <jacky.kschou@gmail.com>
2014-11-13 09:05:36 -0600

    added SpawnProjectileTowardsTarget TargetEffectApplier, refactoring

---
+++ UnityProject/Assets/Scripts/GameScripts/GameLogic/TargetEffectAppliers/Editor/SpawnProjectileTowardsTargetInspector.cs
@@ -0,0 +1,18 @@
+﻿using Assets.Scripts.Constants;
+using UnityEditor;
+
+namespace Assets.Scripts.GameScripts.GameLogic.TargetEffectAppliers.Editor
+{
+    [CustomEditor(typeof(SpawnProjectileTowardsTarget))]
+    public class SpawnProjectileTowardsTargetInspector : TargetEffectApplierInspector 
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            SpawnProjectileTowardsTarget spawnProjectileTowardsTarget = (SpawnProjectileTowardsTarget)target;
+
+            spawnProjectileTowardsTarget.Prefab = (Prefab)EditorGUILayout.EnumPopup("Prefab", spawnProjectileTowardsTarget.Prefab);
+        }
+    }
+}
---
+++ UnityProject/Assets/Scripts/GameScripts/GameLogic/TargetEffectAppliers/SpawnProjectileTowardsTarget.cs
@@ -0,0 +1,24 @@
+﻿using Assets.Scripts.Constants;
+using Assets.Scripts.Managers;
+using Assets.Scripts.Utility;
+using UnityEngine;
+
+namespace Assets.Scripts.GameScripts.GameLogic.TargetEffectAppliers
+{
+    [AddComponentMenu("TargetEffectApplier/SpawnProjectileTowardsTarget")]
+    public class SpawnProjectileTowardsTarget : TargetEffectApplier
+    {
+        public Prefab Prefab;
+
+        protected override void ApplyEffect(GameObject target)
+        {
+            PrefabManager.Instance.SpawnPrefabImmediate(Prefab, o =>
+            {
+                Vector2 castDirecation = UtilityFunctions.GetDirection(GameView.CenterPosition, target.transform.position);
+                o.TriggerGameScriptEvent(GameScriptEvent.UpdateProjectileDirection, castDirecation);
+                o.TriggerGameScriptEvent(GameScriptEvent.UpdateProjectileTarget, target.transform);
+                o.TriggerGameScriptEvent(GameScriptEvent.ShootProjectile);
+            });
+        }
+    }
+}
