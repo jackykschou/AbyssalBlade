12e6a0698b623019810472302cdef3173cee8948
Bsalch <Brandon.Salch@utexas.edu>
2014-11-14 17:00:42 -0600

    Initial Survival Mode

---
+++ UnityProject/Assets/Scripts/GameScripts/GameLogic/TargetEffectAppliers/TeleportPlayerOnPickUp.cs
@@ -0,0 +1,15 @@
+﻿using Assets.Scripts.Managers;
+using UnityEngine;
+
+namespace Assets.Scripts.GameScripts.GameLogic.TargetEffectAppliers
+{
+    [AddComponentMenu("LevelMechanics/TeleportPlayerOnEnter")]
+    public class TeleportPlayerOnPickUp : TargetEffectApplier
+    {
+        protected override void ApplyEffect(GameObject target)
+        {
+           // Vector3 nextSpawnPos = SurvivalModeManager.Instance.GetNextSpawn();
+           // GameManager.Instance.PlayerMainCharacter.transform.position = new Vector3(nextSpawnPos.x, nextSpawnPos.y, GameManager.Instance.PlayerMainCharacter.transform.position.z);
+        }
+    }
+}
---
+++ UnityProject/Assets/Scripts/Managers/SurvivalModeManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Scripts.GameScripts.GameLogic;
+
+using GameEvent = Assets.Scripts.Constants.GameEvent;
+using GameEventAttribute = Assets.Scripts.Attributes.GameEvent;
+
+namespace Assets.Scripts.Managers
+{
+    public class SurvivalModeManager : GameLogic
+    {
+    }
+}
