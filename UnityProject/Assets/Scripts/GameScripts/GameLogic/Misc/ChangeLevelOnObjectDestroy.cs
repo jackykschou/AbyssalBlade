8bb91823de06aadbb125394a6fcb5124c4576912
Jacky Chou <jacky.kschou@gmail.com>
2014-11-22 03:49:37 -0600

    fixed problem where winning screen is displayed event the player is defeated in boss level

---
+++ UnityProject/Assets/Scripts/GameScripts/GameLogic/Misc/ChangeLevelOnObjectDestroy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using Assets.Scripts.Constants;
+using Assets.Scripts.Managers;
+using UnityEngine;
+
+namespace Assets.Scripts.GameScripts.GameLogic.Misc
+{
+    public class ChangeLevelOnObjectDestroy : GameLogic
+    {
+        public Prefab LevelPrefab;
+        [Range(0, 100f)]
+        public float Delay;
+
+        private bool _levelChanged;
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+            _levelChanged = false;
+        }
+
+        [Attributes.GameScriptEvent(GameScriptEvent.OnObjectDestroyed)]
+        public void OnObjectHasNoHitPoint()
+        {
+            if (!_levelChanged)
+            {
+                _levelChanged = true;
+                GameManager.Instance.ChangeLevel(LevelPrefab);
+            }
+        }
+
+        protected override void Deinitialize()
+        {
+        }
+    }
+}
